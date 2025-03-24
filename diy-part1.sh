a74b500c3bb15480be08df4d0310a27fb2009a22
Sumire-Heanna <37390710+Sumire-Heanna@users.noreply.github.com>
2025-03-24 11:02:09 +0800

    Update diy-part1.sh
    Packages added:
    
    TurboACC
    Argon Theme
    PassWall2

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,16 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# Add TurboACC (Full-cone NAT & Flow Offload)
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
+
+# Add Argon Theme
+cd package && git clone https://github.com/jerrykuku/luci-theme-argon.git && cd ..
+
+# Add PassWall2
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+echo 'src-git passwall2packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
+
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
